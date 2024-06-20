7aa884f30b43efbb1b37efcbe41cf4c40ad43b5a
Antony Joseph Raj <antonyjosephraj@jmangroup.com>
2024-06-20 19:05:58 +0530

    Added source files and logic updated

--- pages/source_data.py
+++ pages/source_data.py
@@ -1,16 +1,95 @@
 import streamlit as st
+from streamlit_shap import st_shap
+import shap
+from sklearn.model_selection import train_test_split
+import numpy as np
 import pandas as pd
 
-st.title('SOURCE DATA')      
 
-st.subheader('Investments:')      
-investments=pd.read_csv('./inputs/investments.csv') 
-st.write(investments.head())
 
-st.subheader('Investments Details:')     
-investments_details=pd.read_csv('./inputs/investments_details.csv')    
-st.write(investments_details.head())
+# Investments
+st.subheader(':blue[Investments:]')
+investments = pd.read_csv('./inputs/investments.csv')
+investments_edited_df = st.data_editor(investments)
 
-st.subheader('Assumptions:')     
-assumptions=pd.read_csv('./inputs/assumptions.csv')    
-st.write(assumptions)
+# Investments Details
+st.subheader(':blue[Investments Details:]')      
+investments_details = pd.read_csv('./inputs/investments_details.csv') 
+investments_details_edited_df = st.data_editor(investments_details)
+# investments_details_edited_df = st.data_editor(investments_details, num_rows="dynamic")
+
+# Calculations
+investments_edited_df['Date of Investment'] = pd.to_datetime(investments_edited_df['Date of Investment'], format='%d-%m-%Y').dt.strftime('%Y-%m-%d')
+min_date = investments_edited_df['Date of Investment'].min()
+investments_at_entry = investments_edited_df['Investment at entry'].sum()
+
+# print('investments_edited_df \n', investments_edited_df['Date of Investment'])
+# print('min_date \n', min_date)
+
+
+investments_details_edited_df['Exit Date'] = pd.to_datetime(investments_details_edited_df['Exit Date'], format='%m/%d/%Y').dt.strftime('%Y-%m-%d')
+max_date = investments_details_edited_df['Exit Date'].max()
+# print('investments_details_edited_df \n', investments_details_edited_df['Exit Date'])
+# print('max_date \n', max_date)
+
+# Assumptions
+st.subheader(':blue[Casual Assumptions:]')      
+# columns = ["Date" ,"Low Case" ,"Base Case" ,"High Case" ,"Comments"]
+# assumptions = pd.DataFrame(columns=columns)
+# st.write(assumptions)
+
+date_range = pd.date_range(start=min_date, end=max_date, freq='M')
+assumptions = pd.DataFrame(date_range, columns=['Date'])
+assumptions['Date'] = pd.to_datetime(assumptions['Date'], format='%Y-%m-%d').dt.strftime('%Y-%m-%d')
+assumptions[["Low Case" ,"Base Case" ,"High Case" ,"Comments"]] = None
+assumptions.loc[0, ["Low Case" ,"Base Case" ,"High Case"]] = [-investments_at_entry ,-investments_at_entry ,-investments_at_entry]
+# st.write(assumptions)
+assumptions_edited_df = st.data_editor(assumptions)
+
+st.subheader(':blue[Valuation Waterfall Output:]')
+
+
+column_values_list = investments_details_edited_df['Scenario'].tolist()
+column_values_values = investments_details_edited_df['EBITDA at Exit'].tolist()
+column_values_values2 = investments_details_edited_df['Multiple at Exit'].tolist()
+
+
+# st.write(column_values_list)
+
+data2 = {
+    'Calc': ['ARR /Rev /EBITDA', 'Multiple'],
+    'Entry': [200,10]
+}
+data_v2 = pd.DataFrame(data2)
+# data_v2 = pd.DataFrame(columns=column_values_list)
+data_v2[column_values_list] = None
+data_v2.loc[0, column_values_list] = [column_values_values]
+data_v2.loc[1, column_values_list] = [column_values_values2]
+
+# st.write(data_v2)
+
+data3 = {
+    'Calc': ['Net Debt', 'Cash flow adj'],
+    'Entry': [None,None]
+}
+data_v3 = pd.DataFrame(data3)
+data_v3[column_values_list] = None
+# data_v3_edited_df = st.data_editor(data_v3, hide_index=-1)
+
+# data4 = {
+#     'Calc': ['Equity'],
+#     'Entry': [None]
+# }
+# data_v4 = pd.DataFrame(data4)
+# st.write(data_v2)
+
+
+
+# with st.container(height=300, border=True, backgroundColor='red'):
+with st.container(height=300, border=True):
+    st.write(data_v2)
+
+    data_v3_edited_df = st.data_editor(data_v3, hide_index=-1)
+
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-st.title('STREAMLIT APP')      
 
-st.subheader("Examining the investment funds of a private equity firm includes reviewing specifics of portfolio companies such as initial investment size, EBITDA, and valuation ratios. Comprehending these metrics aids in evaluating the investment's effectiveness and its potential for returns")      
+
+st.markdown(
+    "<h1 style='color: darkblue;'>STREAMLIT APP</h1>",
+    unsafe_allow_html=True
+)
+
+
+st.markdown(
+    "<h4 style='color: pink;'>Examining the investment funds of a private equity firm includes reviewing specifics of portfolio companies such as initial investment size, EBITDA, and valuation ratios. Comprehending these metrics aids in evaluating the investment's effectiveness and its potential for returns</h4>",
+    unsafe_allow_html=True
+)
